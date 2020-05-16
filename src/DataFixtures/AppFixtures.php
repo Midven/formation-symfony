690ee8dd900c519db6f32b270e7c23c6798a0f48
Vincent <vincent.midlaire@gmail.com>
2020-05-16 13:03:25 +0200

    Ajout de la logique des rôles et de la sécurité

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use App\Entity\Role;
 use App\Entity\User;
 use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -23,6 +24,22 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create('fr-FR'); // faker en français
 
+
+        // Création d'un compte administrateur
+        $adminRole = new Role();
+        $adminRole->setTitle('ROLE_ADMIN');
+        $manager->persist($adminRole);
+
+        $adminUser = new User();
+        $adminUser->setFirstName('Vincent')
+                    ->setLastName('Midlaire')
+                    ->setEmail('vincent.midlaire@gmail.com')
+                    ->setHash($this->encoder->encodePassword($adminUser, 'password'))
+                    ->setPicture('https://randomuser.me/api/portraits/men/3.jpg')
+                    ->setIntroduction($faker->sentence())
+                    ->setDescription('<p>' . join('</p><p>', $faker->paragraphs(3)) . '</p>')
+                    ->addUserRole($adminRole);
+        $manager->persist($adminUser);
 
         $users = [];
         $genres = ['male', 'female'];
